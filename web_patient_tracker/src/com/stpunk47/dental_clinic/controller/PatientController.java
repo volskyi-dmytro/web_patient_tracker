862527c496c40d675f7626ffcf10995b984b9866
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-03-03 17:08:36 +0200

    Updating Controller by adding DAO

--- web_patient_tracker/src/com/stpunk47/dental_clinic/controller/PatientController.java
+++ web_patient_tracker/src/com/stpunk47/dental_clinic/controller/PatientController.java
@@ -2,16 +2,35 @@ package com.stpunk47.dental_clinic.controller;
 
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.stpunk47.dental_clinic.dao.PatientDAO;
+import com.stpunk47.dental_clinic.entity.Patient;
 
 @Controller
 @RequestMapping("/patient")
 public class PatientController {
 	
+	//inject patient DAO
+	@Autowired
+	private PatientDAO patientDAO;
+	
+	
 	@RequestMapping("/list")
 	public String listPatients(Model theModel) {
+		//get patient from the DAO
+		List<Patient> thePatients = patientDAO.getPatients();
+		
+		
+		//add clients to the model
+		theModel.addAttribute("patients", thePatients);
+		
+		
 		return "list-patients";
 	}
 
