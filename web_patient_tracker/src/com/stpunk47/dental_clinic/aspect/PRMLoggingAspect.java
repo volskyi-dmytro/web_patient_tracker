dbbb5e74f0c437c896368313d4725f77430dde3c
volskyi.dmytro <volskyi.dmytro@gmail.com>
2020-03-13 14:30:08 +0200

    added @AfterReturning & @Before logs

--- web_patient_tracker/src/com/stpunk47/dental_clinic/aspect/PRMLoggingAspect.java
+++ web_patient_tracker/src/com/stpunk47/dental_clinic/aspect/PRMLoggingAspect.java
@@ -2,7 +2,10 @@ package com.stpunk47.dental_clinic.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -15,26 +18,52 @@ public class PRMLoggingAspect {
 	
 	
 	//setup pointcut declarations
-	@Pointcut("execution(* com.stpunk47.dental_clinic.controller.*.*.(..))")
+	@Pointcut("execution(* com.stpunk47.dental_clinic.controller.*.*(..))")
 	private void forControllerPackage() {}
 	
 	//the same for DAO and service
 	
-	@Pointcut("execution(* com.stpunk47.dental_clinic.service.*.*.(..))")
+	@Pointcut("execution(* com.stpunk47.dental_clinic.service.*.*(..))")
 	private void forServicePackage() {}
 	
-	@Pointcut("execution(* com.stpunk47.dental_clinic.dao.*.*.(..))")
+	@Pointcut("execution(* com.stpunk47.dental_clinic.dao.*.*(..))")
 	private void forDAOPackage() {}
 	
 	@Pointcut("forControllerPackage() || forServicePackage() || forDAOPackage() ")
 	private void forAppFlow() {}
-	
-	
-	
-	
+			
 	
 	//@Before
 	
-	//@AfterReturning
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		//display method
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("===> in @Before: calling method: "+theMethod);
+			
+		//display the arguments
+			Object[] args = theJoinPoint.getArgs();
 
+			for(Object tempArg: args) {
+				myLogger.info("====> argument: "+ tempArg);
+			}
+	}
+	
+	
+	
+	//@AfterReturning
+	
+	@AfterReturning(
+			pointcut = "forAppFlow()",
+			returning = "theResult"
+			)
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("===> in @AfterReturning: calling method: "+theMethod);
+		
+		myLogger.info("====> result: "+theResult);
+		
+	}
+ 
 }
